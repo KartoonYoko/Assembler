3cc944a337a14ae410713c1fc99358d759cf6a0f
KartoonYoko <crashed9001@gmail.com>
2019-12-26 19:24:54 +0900

    aasd

--- assm/assm432.cpp
+++ assm/assm432.cpp
@@ -12,12 +12,26 @@ int main()
     short x = 1;
     short a1 = 1;
     short a2 = 2;
+    short result = 0;
 
     __asm {
 
-        ; начало процедуры
+        ; начало процедуры (AX (хранит x), BX ( для получения данных из стека), CX (степень), DX (РЕЗУЛЬТАТ), SI)
     proc_p:
+        mov cx, 2; степень
+        mov si, ax; переносим x в si для возведения в степень
+        call power_p
+        
+        pop bx
+        imul si, bx
+        mov dx, 0
+        add dx, si; 
 
+        pop bx
+        imul ax, bx
+
+        add dx, ax; результат в DX
+        
         
 
         ret
@@ -29,7 +43,8 @@ int main()
         mov di, si
     pow:
         imul di, si
-        loop pow       
+        loop pow    
+        ret
         ; конец процедуры возведения в степень
 
 
@@ -38,8 +53,27 @@ int main()
     ; здесь можно реализовать ввод по одному значению и записывать его в стек
     mov bx, a1
     mov dx, a2
-    push bx
     push dx
+    push bx
+       
+    mov di, 0; di будет хранить ответ всей задачи
+    call proc_p
+    add di, dx
+    
+    ; повторная запись в стек
+    mov bx, a1
+    mov dx, a2
+    push dx
+    push bx
+
+
+    add ax, -1
+
+    call proc_p
+    add di, dx
+
+    mov result, di; ОТВЕТ
+
 
     }
 }
