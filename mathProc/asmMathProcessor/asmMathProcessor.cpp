a609095cd5221558885acb57806fb088fc065b4f
KartoonYoko <crashed9001@gmail.com>
2019-12-26 20:46:24 +0900

    aasd

--- mathProc/asmMathProcessor/asmMathProcessor.cpp
+++ mathProc/asmMathProcessor/asmMathProcessor.cpp
@@ -4,6 +4,47 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    /*
+        Task #16
+
+        Найти площадь окружности.
+
+        c = 2 * r * PI
+
+        s= PI * r^2
+
+
+    */
+
+    setlocale(LC_ALL, "Rus");
+
+    float pi = 3.14;
+    float c; // длина окружности
+    float result = 0;
+    float two = 2;
+
+    std::cout << "Введите длину окружности c = ";
+    std::cin >> c;
+
+
+
+    //fmul st(2), st(4)
+
+    __asm {
+        fld [c]; st(0)
+        fld [pi]
+        fld [two]
+
+        fdiv st(0), st(1)
+        fdiv st(0), st(2)
+        fmul st(0), st(0)
+        fmul st(0), st(1)
+
+
+        fst [result]
+
+    }
+
+    std::cout << std::endl << "Ответ = " << result;
 }
 
